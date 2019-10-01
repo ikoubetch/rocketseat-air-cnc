8f9339c76cd5b4d8fcd2db60447a81f1a2bd0b70
Igor Koubetch <i.koubetch@gmail.com>
2019-10-01 20:26:05 -0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect(process.env.MongoConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+
+app.listen(3333);
