917fa8b2fa7c02be488e9a6467c5163cf385f8a6
Igor Koubetch <i.koubetch@gmail.com>
2019-10-04 07:56:56 -0300

    alterações backend, codigo frontend e hello word em react native

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 require('dotenv/config');
 
 const routes = require('./routes');
@@ -11,7 +13,9 @@ mongoose.connect(process.env.MongoConnection, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files',express.static(path.resolve(__dirname,'..','uploads')))
 app.use(routes);
 
 
